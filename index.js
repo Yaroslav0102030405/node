a20302a699a3a58f98373783cf2b4ca3cab7c8ce
Yaroslav0102030405 <you@example.com>
2025-03-21 21:04:41 +0200

    lesson 2 start

--- index.js
+++ index.js
@@ -12,6 +12,46 @@
 // console.log(users.admin);
 
 // новий варіант импорту
-import users from './users.js';
-console.log(users.admin);
-console.log(users.clients);
+// import users from './users.js';
+// console.log(users.admin);
+// console.log(users.clients);
+
+// Робота з файловою системою
+// const fs = require('fs');
+
+// // прочитати файл
+// fs.readFile('read.txt', 'utf-8', (error, data) => {
+//   if (error) {
+//     console.log(error.message);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+const fs = require('fs').promises;
+
+// const readFile = async (fileName) => {
+//   try {
+//     const data = await fs.readFile(fileName, 'utf-8');
+//     const newData = `${data}\nИ ни слова про Эксклера`;
+//     await fs.writeFile(fileName, newData);
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
+
+// readFile('read.txt');
+
+const readFile2 = async (fileName) => {
+  try {
+    // const data = await fs.readFile(fileName, 'utf-8');
+    const newData2 = `\nОн считает Ницше`;
+    await fs.appendFile(fileName, newData2);
+    // console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+readFile2('read.txt');
---
+++ promise-index.js
@@ -0,0 +1,14 @@
+// const fs = require('fs').promises;
+
+// const readFile = async (fileName) => {
+//   try {
+//     const data = await fs.readFile(fileName, 'utf-8');
+//     const newData = `${data}\nИ ни слова про Эксклера`;
+//     await fs.writeFile(fileName, newData);
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
+
+// readFile('read.txt');
